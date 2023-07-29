d3b497710ade61db4b9bbb9d47887f3ad164c95e
João Junior <batistasousajoao@gmail.com>
2023-07-28 21:34:09 -0300

    Adicionar arquivos de projeto.

---
+++ funcionarios1/Program.cs
@@ -0,0 +1,39 @@
+﻿using funcionarios1;
+using System;
+using System.Globalization;
+
+namespace funcionarios
+{
+    public class _funcionarios
+    {
+        static void Main(string[] args)
+        {
+
+            Funcionario funcionario1;
+            Funcionario funcionario2;
+
+            funcionario1 = new Funcionario();
+            funcionario2 = new Funcionario();
+
+            Console.WriteLine("Digite os dados do funcionário: ");
+            Console.Write("Nome: ");
+            funcionario1.funcionarioNome = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionario1.funcionarioSalario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite os dados do funcionario: ");
+            Console.Write("Nome: ");
+            funcionario2.funcionarioNome = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionario2.funcionarioSalario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double mediaSalarial = (funcionario1.funcionarioSalario + funcionario2.funcionarioSalario) / 2.0;
+
+            Console.WriteLine("O salário médio dos funcionários é: " + mediaSalarial.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+
+        }
+    }
+}
